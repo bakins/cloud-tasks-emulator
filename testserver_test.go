db2c529accaf19e5932d63797e291ce9df915a92
Brian Akins <205350+bakins@users.noreply.github.com>
2022-04-29 15:03:06 -0400

    Testing

--- testserver_test.go
+++ testserver_test.go
@@ -2,7 +2,10 @@ package emulator
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
 	"github.com/stretchr/testify/require"
@@ -17,10 +20,13 @@ func TestTestServer(t *testing.T) {
 
 	require.NotEmpty(t, svr.Address())
 
-	conn, err := grpc.Dial(svr.Address(), grpc.WithInsecure())
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, svr.Address(), grpc.WithInsecure())
 	require.NoError(t, err)
 
-	client, err := cloudtasks.NewClient(context.Background(), option.WithGRPCConn(conn))
+	client, err := cloudtasks.NewClient(ctx, option.WithGRPCConn(conn))
 	require.NoError(t, err)
 
 	createQueueRequest := taskspb.CreateQueueRequest{
@@ -28,8 +34,40 @@ func TestTestServer(t *testing.T) {
 		Queue:  newQueue(formattedParent, "test"),
 	}
 
-	queue, err := client.CreateQueue(context.Background(), &createQueueRequest)
+	queue, err := client.CreateQueue(ctx, &createQueueRequest)
 	require.NoError(t, err)
 
 	require.Equal(t, createQueueRequest.Queue.Name, queue.Name)
+
+	done := make(chan struct{})
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		close(done)
+	}
+
+	target := httptest.NewServer(http.HandlerFunc(handler))
+	defer target.Close()
+
+	createTaskRequest := taskspb.CreateTaskRequest{
+		Parent: queue.GetName(),
+		Task: &taskspb.Task{
+			MessageType: &taskspb.Task_HttpRequest{
+				HttpRequest: &taskspb.HttpRequest{
+					Url: target.URL,
+				},
+			},
+		},
+	}
+
+	createdTask, err := client.CreateTask(ctx, &createTaskRequest)
+	require.NoError(t, err)
+
+	require.Equal(t, taskspb.HttpMethod_POST, createdTask.GetHttpRequest().GetHttpMethod())
+	require.EqualValues(t, 0, createdTask.GetDispatchCount())
+
+	select {
+	case <-done:
+	case <-ctx.Done():
+		t.Errorf("failed to receive task")
+	}
 }
